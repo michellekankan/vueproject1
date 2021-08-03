3f1b342a87baf51b319da2a485fb18fb17ab323f
michelle <michellekan0102@gmail.com>
2021-08-03 20:04:46 +0800

    first update

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created() {
+    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+    //API伺服器路徑
+    //所申請的APIPATH
+    console.log(process.env.APIPATH, process.env.CUSTOMPATH);
+    this.$http.get(api).then((res) => {
+      console.log(res.data)
+    })
+    
+  },
+}
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+
+</style>
